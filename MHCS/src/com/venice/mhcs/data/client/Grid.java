47bd5aa434e543392e8cac4cb683561f4e0ddc25
effin007 <effin007@d.umn.edu>
2015-04-12 17:47:15 -0500

    I did some more changes to the Grid class, it checks to make sure things aren't attempted to be placed out of bounds.  I didn't do anything complicated.  I think i'm going to make a class to replace my "Object" calls which can be either "terrain" or "module" I don't know what it will look like yet, so i'm going to take a break and try to figure that out

--- MHCS/src/com/venice/mhcs/data/client/Grid.java
+++ MHCS/src/com/venice/mhcs/data/client/Grid.java
@@ -1,34 +1,85 @@
 package com.venice.mhcs.data.client;
 
 public class Grid {
-	public Grid(int GridLength,
-				int GridHeight)
-				{
-					length = GridLength;
-					height = GridHeight;
-					grid = new Object[GridHeight][GridLength];
-				}
+	/**
+	 * Constructor that is called when specific dimensions for a grid are required.
+	 * @param GridLength
+	 * @param GridHeight
+	 */
+	public Grid(int GridLength, int GridHeight)
+	{
+		length = GridLength;
+		height = GridHeight;
+		grid = new Object[GridHeight][GridLength];
+	}
+	/**
+	 * Constructor called when no dimensions given.
+	 */
 	public Grid()
 	{
 		length = 100;
 		height = 50;
-		grid = new Object[50][100];
+		grid = new Object[height][length];
 	}
 	
+	/**
+	 * Function that returns the length of the grid.
+	 * @return length
+	 */
 	public int getLength()
 	{
 		return length;
 	}
 	
+	/**
+	 * Function that returns the height of the grid.
+	 * @return height
+	 */
 	public int getHeight()
 	{
 		return height;
 	}
 	
-	public Object getItem(int xcoord,
-						  int ycoord)
+	/**
+	 * returns the object at the given dimensions, returns null if out-of-bounds or doesn't exist
+	 * @param xcoord
+	 * @param ycoord
+	 * @return object 
+	 */
+	public Object getItem(int xcoord, int ycoord)
 	{
-		return grid[xcoord][ycoord];
+		Object module;
+		if(xcoord < length && xcoord >= 1 && ycoord < height && ycoord >= 1)
+		{
+			module = grid[xcoord - 1][ycoord - 1];
+		}
+		else
+		{
+			module = null;
+		}
+		return module;
+	}
+	
+	/**
+	 * Function that sets the object in the grid to the given object at the given coordinates.
+	 * @param xcoord
+	 * @param ycoord
+	 * @param module
+	 * @return boolean 
+	 */
+	public boolean setItem(int xcoord, int ycoord, Object module)			   
+	{
+		boolean result;
+		if(xcoord < length && xcoord >= 1 && ycoord < height && ycoord >= 1)
+		{
+			grid[xcoord - 1][ycoord - 1] = module;
+			result = true;
+		}
+		else
+		{
+			result = false;
+		}
+		return result;
 	}
 	private int length, height;
 	private Object[][] grid;
