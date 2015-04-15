f629e70f564f8327f9f90b5e3f600ed4d7f1e6e7
gunna025 <gunna025@d.umn.edu>
2015-04-15 17:32:15 -0500

    Updated Module constructor

--- MHCS/test/moduleTesting/ConsistencyTest.java
+++ MHCS/test/moduleTesting/ConsistencyTest.java
@@ -6,7 +6,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.venice.mhcs.data.client.Module;
-import com.venice.mhcs.data.client.ModuleType;
 
 public class ConsistencyTest {
 
@@ -15,8 +14,8 @@ public class ConsistencyTest {
 		Module m1 = new Module();
 		
 		m1.setID("40");
-		m1.setDamage("Good");
-		m1.setModuleType("Plain");
+		m1.setDamage("Undamaged");
+		m1.setModuleType("40");
 		m1.setRotation("2");
 		m1.setX("31");
 		m1.setY("13");
@@ -30,7 +29,7 @@ public class ConsistencyTest {
 		
 		m2.setID("0");
 		m2.setDamage("Good");
-		m2.setModuleType("Plain");
+		m2.setModuleType("0");
 		m2.setRotation("2");
 		m2.setX("31");
 		m2.setY("13");
@@ -44,7 +43,7 @@ public class ConsistencyTest {
 		
 		m3.setID("40");
 		m3.setDamage("asdf");
-		m3.setModuleType("Plain");
+		m3.setModuleType("40");
 		m3.setRotation("2");
 		m3.setX("31");
 		m3.setY("13");
@@ -58,7 +57,7 @@ public class ConsistencyTest {
 		
 		m4.setID("40");
 		m4.setDamage("Good");
-		m4.setModuleType("Plain");
+		m4.setModuleType("40");
 		m4.setRotation("2");
 		m4.setX("0");
 		m4.setY("13");
@@ -72,7 +71,7 @@ public class ConsistencyTest {
 		
 		m5.setID("40");
 		m5.setDamage("Good");
-		m5.setModuleType("Plain");
+		m5.setModuleType("40");
 		m5.setRotation("2");
 		m5.setX("31");
 		m5.setY("0");
@@ -86,7 +85,7 @@ public class ConsistencyTest {
 		
 		m6.setID("40");
 		m6.setDamage("Good");
-		m6.setModuleType("Plain");
+		m6.setModuleType("40");
 		m6.setRotation("3");
 		m6.setX("31");
 		m6.setY("13");
@@ -102,31 +101,28 @@ public class ConsistencyTest {
 		assertFalse(m7.setID("0"));
 		assertFalse(m7.setID("190"));
 		
-		assertTrue(m7.setDamage("Good"));
-		assertTrue(m7.setDamage("Moderate"));
-		assertTrue(m7.setDamage("Bad"));
+		assertTrue(m7.setDamage("Undamaged"));
+		assertTrue(m7.setDamage("Damaged"));
+		assertTrue(m7.setDamage("Uncertain"));
 		assertFalse(m7.setDamage("adsf"));
 		assertFalse(m7.setDamage("good"));
 		
 		assertTrue(m7.setX("1"));
 		assertTrue(m7.setX("50"));
 		assertFalse(m7.setX("0"));
-		assertFalse(m7.setX("51"));
+		assertFalse(m7.setX("101"));
 		
 		assertTrue(m7.setY("1"));
 		assertTrue(m7.setY("50"));
 		assertFalse(m7.setY("0"));
 		assertFalse(m7.setY("51"));
 		
-		assertTrue(m7.setModuleType("Plain"));
-		assertTrue(m7.setModuleType("Airlock"));
+		assertTrue(m7.setModuleType("40"));
+		assertTrue(m7.setModuleType("65"));
 		
 		assertTrue(m7.setRotation("0"));
 		assertTrue(m7.setRotation("1"));
 		assertTrue(m7.setRotation("2"));
-		assertFalse(m7.setRotation("50"));
-
-
-		
-	}	
+		assertFalse(m7.setRotation("50"));		
+	}
 }
