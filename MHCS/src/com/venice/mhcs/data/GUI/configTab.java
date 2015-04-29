4cc765b8bc12dca0a1c4c79d0bcb8a4cd9e553cd
geesm005 <geesm005@d.umn.edu>
2015-04-29 17:14:45 -0500

    i was told to push my config tab of fun and butterflies

--- MHCS/src/com/venice/mhcs/data/GUI/configTab.java
+++ MHCS/src/com/venice/mhcs/data/GUI/configTab.java
@@ -1,5 +1,7 @@
 package com.venice.mhcs.data.GUI;
 
+import java.util.ArrayList;
+
 import javax.validation.constraints.Min;
 
 import com.google.gwt.resources.css.Minify;
@@ -9,6 +11,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Panel;
 import com.venice.mhcs.data.client.Configuration;
 import com.venice.mhcs.data.client.LocalStorage;
+import com.venice.mhcs.data.client.Module;
 import com.venice.mhcs.data.client.ModuleGrid;
 import com.venice.mhcs.data.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
@@ -42,8 +45,40 @@ public class configTab {
 	private static ModuleGrid grid = new ModuleGrid();
 	private static ScrollPanel sp = new ScrollPanel();
 	private static ModuleMap map = new ModuleMap();
-	
+	private static ArrayList<Module> modList = new ArrayList<Module>();
 	public static void initConfig(){
+		
+		final ListBox arrayBox = new ListBox();
+		arrayBox.addItem("Local Storage");
+		arrayBox.addItem("Test1");
+		arrayBox.addItem("Test2");
+		arrayBox.addItem("Test3");
+		arrayBox.addItem("Test4");
+		arrayBox.addItem("Test5");
+		arrayBox.addItem("Test6");
+		arrayBox.addItem("Test7");
+		arrayBox.addItem("Test8");
+		arrayBox.addItem("Test9");
+		arrayBox.addItem("Test10");
+		
+		arrayBox.setStylePrimaryName("listBoxMargin");
+		
+		final ListBox configBox = new ListBox();
+		
+		configBox.addItem("Minimum Config1");
+		configBox.addItem("Minimum Config2");
+		configBox.setStylePrimaryName("listBoxMargin");
+		
+		final Button sumbitButton = new Button("Sumbit");
+		sumbitButton.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				
+			}
+		});
+		
 		mini.setMinimum1(stor.getModuleList());
 		
 		if(mini.isMinimum(mini.getMinimum1())){
